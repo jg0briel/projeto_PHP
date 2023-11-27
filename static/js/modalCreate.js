9f0bf12e7265fd90ab196ebf7b5c75fa5dde7b33
Júlio César <julioceasarcs@gmail.com>
2023-11-27 15:01:00 -0300

    add hero funcional

--- static/js/modalCreate.js
+++ static/js/modalCreate.js
@@ -20,16 +20,38 @@ function closeModalCreate() {
   }, 300);
 }
 
-function handleSubmit(e) {
-  e.preventDefault();
-  closeModalCreate(); 
-}
+// function handleSubmit(e) {
+//   e.preventDefault();
+//   closeModalCreate(); 
+// }
 
 document.querySelector('.open-modal-btn-create').addEventListener('click', openModalCreate);
 
-document.querySelector('.content-create form').addEventListener('submit', handleSubmit);
+// document.querySelector('.content-create form').addEventListener('submit', handleSubmit);
 
 function resizeTextArea(textarea) {
   textarea.style.height = 'auto';
   textarea.style.height = textarea.scrollHeight + 'px'; 
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const createHero = document.getElementById('createHero');
+
+  createHero.addEventListener('submit', function (event) {
+      event.preventDefault();
+      closeModalCreate();
+
+      const formData = new FormData(createHero);
+
+      fetch('../Crud/create.php', {
+          method: 'POST',
+          body: formData
+      })
+      .then(data => {
+          console.log('Sucesso:', data);
+      })
+      .catch(error => {
+          console.error('Erro:', error);
+      });
+  });
+});

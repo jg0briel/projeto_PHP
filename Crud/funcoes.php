69e2b30c9652c0b75a44ca0d7079a9b492181990
Perilocc <perilojp@gmail.com>
2023-11-28 09:24:05 -0300

    Adaptação da func read

--- Crud/funcoes.php
+++ Crud/funcoes.php
@@ -49,6 +49,69 @@ function read_characters()
     }
 }
 
+function read_heroes()
+{
+    global $conexao;
+    $sql = "SELECT * FROM heroes WHERE funcao LIKE 'H%' OR 'h%';";
+    $result = $conexao->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "<section class='card'>";
+            echo "<div class='content-data'>";
+            echo "<p>Nome: " . $row["nome_heroi"] . "</p>";
+            echo "<p>Identidade Civil: " . $row["identidade_civil"] . "</p>";
+            echo "<p>Habilidades: " . $row["habilidades"] . "</p>";
+            echo "<p class='history'>História: " . $row["historia"] . "</p>";
+            echo "<p>Universo: " . $row["universo"] . "</p>";
+            echo "<p>Função: " . $row["funcao"] . "</p>";
+            echo "</div>";
+            echo "<div class='delete'>";
+            echo "<button class='open-modal-btn-delete'><img src='../static/images/delete.png' alt='Apagar'></button>";
+            echo "</div>";
+            echo "<div class='edit'>";
+            echo "<button class='open-modal-btn' data-id='" . $row['id'] . "'><img src='../static/images/edit.png' alt='Editar'></button>";
+            echo "</div>";
+            echo "</div>";
+            echo "</section>";
+        }
+    } else {
+        echo "Sem resultado";
+    }
+}
+
+function read_villains()
+{
+    global $conexao;
+    $sql = "SELECT * FROM heroes WHERE funcao LIKE 'V%' OR 'v%'";
+    $result = $conexao->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "<section class='card'>";
+            echo "<div class='content-data'>";
+            echo "<p>Nome: " . $row["nome_heroi"] . "</p>";
+            echo "<p>Identidade Civil: " . $row["identidade_civil"] . "</p>";
+            echo "<p>Habilidades: " . $row["habilidades"] . "</p>";
+            echo "<p class='history'>História: " . $row["historia"] . "</p>";
+            echo "<p>Universo: " . $row["universo"] . "</p>";
+            echo "<p>Função: " . $row["funcao"] . "</p>";
+            echo "</div>";
+            echo "<div class='delete'>";
+            echo "<button class='open-modal-btn-delete'><img src='../static/images/delete.png' alt='Apagar'></button>";
+            echo "</div>";
+            echo "<div class='edit'>";
+            echo "<button class='open-modal-btn' data-id='" . $row['id'] . "'><img src='../static/images/edit.png' alt='Editar'></button>";
+            echo "</div>";
+            echo "</div>";
+            echo "</section>";
+        }
+    } else {
+        echo "Sem resultado";
+    }
+}
+
+
 function update_character($id, $nome, $identidade_civil, $habilidades, $historia, $universo, $funcao) {
 
     global $conexao;

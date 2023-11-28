cb04ea551a07bc56df581896fcf08cae0ee6e720
Gabriel Montenegro <131418339+GabrielMontenegrOL@users.noreply.github.com>
2023-11-28 20:04:59 -0300

    Add files via upload

---
+++ Crud/conection.php
@@ -0,0 +1,16 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "heroes_project";
+
+    // Estabelece conexão com o banco
+    $conexao = new mysqli($servername, $username, $password, $dbname);
+
+    // Verificação
+    if ($conexao->connect_error) {
+        die("Conexão falhou: " . $conexao->connect_error);
+    } 
+    else {
+    }
+?>
